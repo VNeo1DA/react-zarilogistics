388f35c19e3b5bc2f7a73415346e5019b0bb7732
VNeo1DA <81417465+VNeo1DA@users.noreply.github.com>
2024-05-27 11:31:23 +0200

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-/*import './index.css'*/
 import { 
   createBrowserRouter,
   createRoutesFromElements,  
   Route, 
   RouterProvider
 } from 'react-router-dom';
-//data needed by Routes
-import profileData from './data/company-profile.json'
 
 //Routes
 import Home, {profileLoader} from './pages/Home';
@@ -16,10 +13,8 @@ import Customs from './pages/Customs'
 import NotFound from './pages/NotFound';
 import Other from './pages/Other';
 import Contact, {contactAction} from './pages/Contact';
-//Layout Routes
+//Layout Route
 import RootLayout from './layouts/RootLayout';
-
-//import Other from './layouts/OthersLayout';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -36,13 +31,11 @@ const router = createBrowserRouter(
       </Route>
       <Route path="contact" element={<Contact />} action={contactAction}/>
       <Route path="*" element={<NotFound />} />
-
     </Route>
   )
 )
 
 function App() {
-
   return (
       <RouterProvider router={router} />
   )

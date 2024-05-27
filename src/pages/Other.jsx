b8709bffd155142a52d3bbbf74c658ffb41a2120
VNeo1DA <81417465+VNeo1DA@users.noreply.github.com>
2024-05-27 11:56:57 +0200

    Other.jsx

--- src/pages/Other.jsx
+++ src/pages/Other.jsx
@@ -1,28 +1,27 @@
 import { Outlet, Link } from "react-router-dom";
 function Other(){
-    return(
-        <div className="others-layout">
-            {/*insert hero image*/}
+	return(
+		<div className="others-layout">
 			<div id="hero" className="tablet-desktop other-services2">
 				<img 
-                    src="assets/sea-port-hero.jpg" 
-                    alt="Sea port at night"
-                />
-			</div>
-            <p>
-                All goods that are imported or exported into Southern Africa and other countries 
-                require freight fowarding and customs clearance services. At Zarilogistics we 
-                ensure that storage for cargo is organized for goods, we distribute the cargo 
-                when the time arises to its destination from our storage facilities, and make sure
-                 we handle documentation and processes for customs clearing to help clear your cargo.
-            </p>
+					src="assets/sea-port-hero.jpg" 
+					alt="Sea port at night"
+				/>
+			</div>	
+			<p>
+				All goods that are imported or exported into Southern Africa and other countries 
+				require freight fowarding and customs clearance services. At Zarilogistics we 
+				ensure that storage for cargo is organized for goods, we distribute the cargo 
+				when the time arises to its destination from our storage facilities, and make sure
+				we handle documentation and processes for customs clearing to help clear your cargo.
+			</p>
 
-            <nav>
-                <Link to="customs">Customs Clearance</Link>
-                <Link to="forwarding">Forward Cargo</Link>
-            </nav>
-            <Outlet />
-        </div>
-    );
+			<nav>
+				<Link to="customs">Customs Clearance</Link>
+				<Link to="forwarding">Forward Cargo</Link>
+			</nav>
+			<Outlet />
+		</div>
+	);
 }
 export default Other;

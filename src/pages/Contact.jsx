921168f703f97cdc91c6513faf29613d47f533cb
VNeo1DA <81417465+VNeo1DA@users.noreply.github.com>
2024-05-27 12:05:10 +0200

    Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,55 +1,56 @@
 import { Form, redirect, useActionData } from 'react-router-dom';
 export default function Contact(){
+	const data = useActionData();
+	return(
+		<main>
+			<div id='contact'>
+				<h2>Getting in touch is easy</h2>
+				<p>
+					You can contact Zarilogistics and clearing services today or
+					visit our offices during weekdays:
+				</p>
+				<h4 className="tel-num">(079)320-9844</h4>
+				<h4>Email us:  
+					<a 
+					href="mailto: johnDOE@zarilogistics.co.za" 
+					className="contact-email-link"
+					>
+						johnDOE@zarilogistics.co.za
+                    			</a>
+                		</h4>
+				
+                		<h4>Visit us at: Polofields Estate, 540 Block 90, Max Drive, Waterfall, Gauteng - 1684</h4>
 
-    const data = useActionData();
-    return(
-        <main>
-            <div id='contact'>
-                <h2>Getting in touch is easy</h2>
-                <p>You can contact Zarilogistics and clearing services today or
-                    visit our offices during weekdays:
-                </p>
-                <h4 className="tel-num">(079)320-9844</h4>
-                <h4>Email us:  
-                    <a 
-                        href="mailto: johnDOE@zarilogistics.co.za" 
-                        className="contact-email-link"
-                    > johnDOE@zarilogistics.co.za
-                    </a>
-                </h4>
-                <h4>Visit us at: Polofields Estate, 540 Block 90, Max Drive, Waterfall, Gauteng - 1684</h4>
+                		<Form method="POST" action="/contact" id="form">
+                			<div className="first-last">            
+                				<div className="first">
+                					<label>Name</label><br/>
+                					<input type="text" id="name" placeholder="Full Name" />
+                				</div>
 
-                <Form method="POST" action="/contact" id="form">
-                    <div className="first-last">
-                
-                        <div className="first">
-                            <label>Name</label><br/>
-                            <input type="text" id="name" placeholder="Full Name" />
-                        </div>
+                				<div className="last">
+                					<label>Email</label><br/>
+                					<input type="email" id="email" placeholder="e-mail" />
+                				</div>
+                			</div>
 
-                        <div className="last">
-                            <label>Email</label><br/>
-                            <input type="email" id="email" placeholder="e-mail" />
-                        </div>
-                    </div>
-
-                    <div className="msg-area">
-                        <label>Message</label><br/>
-                        <textarea 
-                            id="msg" 
-                            name="message" 
-                            rows="16" 
-                            cols="50" 
-                            placeholder="Your Message"
-                        >
-                        </textarea>
-                    </div>
-                    <button type="submit">Send Details</button>
-                    {data && data.error && <p>{data.error}</p>}
-                </Form>
-            </div>
-        </main>
-    );
+                			<div className="msg-area">
+                				<label>Message</label><br/>
+                				<textarea 
+                					id="msg" 
+                					name="message" 
+                					rows="16" 
+                					cols="50" 
+                					placeholder="Your Message"
+                				>
+                				</textarea>
+                			</div>
+                			<button type="submit">Send Details</button>
+                			{data && data.error && <p>{data.error}</p>}
+                		</Form>
+			</div>
+		</main>
+	);
 }
 
 export const contactAction = async({ request }) =>{
